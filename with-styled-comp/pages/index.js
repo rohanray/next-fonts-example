e255b24cb1dac5585f892d63c66b8cd88abf1441
Valentino-Houessou <valentino.houessou@gmail.com>
2020-01-30 13:03:34 +0100

    first commit for with-styled-component

---
+++ with-styled-comp/pages/index.js
@@ -0,0 +1,24 @@
+const HomePage = () => {
+  return (
+    <div>
+      <p>Welcome to Next.js!</p>
+
+      <h2>Learn how to add fonts in a React Project</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <hr />
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+    </div>
+  );
+};
+
+export default HomePage;
